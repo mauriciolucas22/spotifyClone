385030e44496db3911c05656e13fb6dfffd6df86
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-04-04 10:55:09 -0300

    pages e routes criadas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
 import 'config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import store from 'store';
+import Routes from 'routes';
 
 const App = () => (
   <Provider store={store}>
-    <View />
+    <Routes />
   </Provider>
 );
 
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,13 @@
 import { StackNavigator } from 'react-navigation';
 
-const Routes = StackNavigator({
+import Main from 'pages/main';
+import Album from 'pages/album';
+import Search from 'pages/search';
 
+const Routes = StackNavigator({
+  Main: { screen: Main },
+  Search: { screen: Search },
+  Album: { screen: Album },
 });
 
 export default Routes;
