8d8d75514d895e886e697a8c2f1d1f586725a6c4
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-04-04 11:34:47 -0300

    header main estilizado com navigate para Search

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import styles from './styles';
 
-const Main = () => <View />;
+const Main = () => (
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+  </View>
+);
+
+Main.navigationOptions = ({ navigation }) => ({
+  title: 'Sua Bibliteca',
+  headerRight: (
+    <TouchableOpacity style={styles.headerRight} onPress={() => {navigation.navigate('Search')}}>
+      <Icon name="search" size={24} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
 
 export default Main;
