7b0b7c2bbc0a8071d312211720298c1d98fd53b6
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-04-06 10:38:01 -0300

    error

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
 
+const currentSong = {
+  title: 'SongTitle',
+  author: 'Author',
+};
+
 const Player = () => (
-  <View />
+  <View style={styles.container}>
+    <View style={styles.currentSong}>
+      <Text style={styles.title}>{currentSong.title}</Text>
+      <Text style={styles.author}>{currentSong.author}</Text>
+    </View>
+
+    <View stlye={styles.controls}>
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="skip-previous" size={24} style={styles.controlIcon} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="play-circle-filled" size={36} style={styles.controlIcon} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="skip-next" size={24} style={styles.controlIcon} />
+      </TouchableOpacity>
+    </View>
+  </View>
 );
 
 export default Player;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import 'config/ReactotronConfig';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
+
+import 'config/ReactotronConfig';
 import store from 'store';
 import Routes from 'routes';
+import Player from 'components/Player';
 
 const App = () => (
   <Provider store={store}>
-    <Routes />
+    <Fragment>
+      <Routes />
+      <Player />
+    </Fragment>
   </Provider>
 );
 
