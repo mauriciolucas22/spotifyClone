0b23f14485ba21b262e4623ea39d051d76dab955
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-04-09 17:28:29 -0300

    state immutable no duck albums ok

--- src/store/ducks/albums.js
+++ src/store/ducks/albums.js
@@ -1,18 +1,18 @@
+import Immutable from 'seamless-immutable';
+
 export const Types = {
   GET_REQUEST: 'albums/GET_REQUEST',
   GET_SUCCESS: 'albums/GET_SUCCESS',
   GET_FAILURE: 'albums/GET_FAILURE',
 };
 
-const initialState = {
+const initialState = Immutable({
   data: [],
   loading: false,
   error: null,
-};
+});
 
 export default function albums(state = initialState, action) {
-  console.tron.log(action);
-
   switch (action.type) {
     case Types.GET_REQUEST:
       return { ...state, loading: true };
