de7d91a64647796a3dc68efe2e18315ccad08c50
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-04-11 08:42:47 -0300

    SongItem dispara action SET_SONG_REQUEST

--- src/components/SongItem/index.js
+++ src/components/SongItem/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -10,30 +10,24 @@ import { Creators as PlayerActions } from 'store/ducks/player';
 
 import styles from './styles';
 
-class SongItem extends Component {
-  static propTypes = {
-    song: PropTypes.shape({
-      title: PropTypes.string,
-      author: PropTypes.string,
-    }).isRequired,
-  };
+const SongItem = ({ song, setSongRequest }) => (
+  <TouchableOpacity style={styles.container} onPress={() => { setSongRequest(song); }}>
+    <View style={styles.songInfo}>
+      <Text style={styles.title}>{song.title}</Text>
+      <Text style={styles.author}>{song.author}</Text>
+    </View>
 
-  render() {
-    const { song } = this.props;
-    return (
-      <View style={styles.container}>
-        <View style={styles.songInfo}>
-          <Text style={styles.title}>{song.title}</Text>
-          <Text style={styles.author}>{song.author}</Text>
-        </View>
+    <Icon name="play-circle-filled" size={20} style={styles.icon} />
+  </TouchableOpacity>
+);
 
-        <TouchableOpacity onPress={() => {}}>
-          <Icon name="play-circle-filled" size={20} style={styles.icon} />
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+SongItem.propTypes = {
+  song: PropTypes.shape({
+    title: PropTypes.string,
+    author: PropTypes.string,
+  }).isRequired,
+  setSongRequest: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = state => ({
   player: state.player,
