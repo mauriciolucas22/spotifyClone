72ea27fbf74ecbc4e7f7f813a8cf8e484934b42d
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-04-11 08:36:33 -0300

    SongItem conectado com duck player

--- src/components/SongItem/index.js
+++ src/components/SongItem/index.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { Creators as PlayerActions } from 'store/ducks/player';
+
 import styles from './styles';
 
-const SongItem = ({ song }) => (
-  <View style={styles.container}>
-    <View style={styles.songInfo}>
-      <Text style={styles.title}>{song.title}</Text>
-      <Text style={styles.author}>{song.author}</Text>
-    </View>
+class SongItem extends Component {
+  static propTypes = {
+    song: PropTypes.shape({
+      title: PropTypes.string,
+      author: PropTypes.string,
+    }).isRequired,
+  };
 
-    <TouchableOpacity onPress={() => {}}>
-      <Icon name="play-circle-filled" size={20} style={styles.icon} />
-    </TouchableOpacity>
-  </View>
-);
+  render() {
+    const { song } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.songInfo}>
+          <Text style={styles.title}>{song.title}</Text>
+          <Text style={styles.author}>{song.author}</Text>
+        </View>
 
-export default SongItem;
+        <TouchableOpacity onPress={() => {}}>
+          <Icon name="play-circle-filled" size={20} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  player: state.player,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(PlayerActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(SongItem);
